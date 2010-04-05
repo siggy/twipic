6ecabc4de2f9125e9ca6aeb9fd0d3ff0c0cb63db
root <andrewseigner@gmail.com>
2010-04-04 21:53:53 -0700

    white space cleanup

--- twipic_controller.rb
+++ twipic_controller.rb
@@ -9,29 +9,29 @@ require 'uri'
 # http://localhost/full/screen_name
 
 class TwipicController < ApplicationController
-	def retrieve
-		profile_image_url = "/404.html"
+  def retrieve
+    profile_image_url = "/404.html"
 
-		if params[:size] != nil && params[:id] != nil
-			resource = Net::HTTP.new("api.twitter.com",80)
-			headers,data = resource.get('/1/users/show/' + params[:id] + '.json')
+    if params[:size] != nil && params[:id] != nil
+      resource = Net::HTTP.new("api.twitter.com",80)
+      headers,data = resource.get('/1/users/show/' + params[:id] + '.json')
 
-			tmp_url = JSON.parse(data)['profile_image_url']
+      tmp_url = JSON.parse(data)['profile_image_url']
 
-			if tmp_url != nil
-				case params[:size]
-				when "normal"
-					profile_image_url = tmp_url
-				when "bigger"
-					profile_image_url = tmp_url
-					profile_image_url["_normal"] = "_bigger"
-				when "full"
-					profile_image_url = tmp_url
-					profile_image_url["_normal"] = ""
-				end
-			end
-		end
+      if tmp_url != nil
+        case params[:size]
+        when "normal"
+          profile_image_url = tmp_url
+        when "bigger"
+          profile_image_url = tmp_url
+          profile_image_url["_normal"] = "_bigger"
+        when "full"
+          profile_image_url = tmp_url
+          profile_image_url["_normal"] = ""
+        end
+      end
+    end
 
-		redirect_to(profile_image_url)
-	end
+    redirect_to(profile_image_url)
+  end
 end
